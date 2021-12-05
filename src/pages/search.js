5dee7418bdb3ce620caefdd8c3f6a2928b0a2049
sungjin <admin@sungjin.dev>
2021-12-05 21:44:52 +0900

    End all search feature

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,8 +1,9 @@
 import { searchPost } from "../api/post/post";
 import { useState } from "react";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Search() {
-    const handleSubmit =  async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const search = await searchPost(e.target.search.value);
         console.log(search)
@@ -12,13 +13,15 @@ export default function Search() {
     const [result, setResult] = useState([]);
 
     return (
-        <div>
+        <div className=" ml-10 mr-10">
             <form onSubmit={handleSubmit}>
-                <input name="search" id="search" placeholder="Search Something..."></input>
-                <button type="submit">Search</button>
+                <input name="search" id="search" placeholder="Search Something..." className="w-[90%] h-10 text-xl"></input>
+                <button type="submit">
+                    <SearchIcon className="h-10 ml-2 -mb-3 rounded-full border-black border-2 p-1" />
+                </button>
             </form>
             <div>
-                <h1>Search Result</h1>
+                <h1 className="text-2xl font-bold">Search Result</h1>
                 {result.map((post) => {
                     return (
                         <div>
